7f84e5ec920fa74865852df10cf86ca92330384a
tobilarscheid <tobilarscheid@gmail.com>
2016-01-30 17:50:00 +0100

    init pebble project

---
+++ src/pebble-jump.c
@@ -0,0 +1,48 @@
+#include <pebble.h>
+
+/**
+ * This is the bare minimum to make a looping game with PGE!
+ */
+
+#include <pebble.h>
+
+#include "pge/pge.h"
+
+static Window *s_game_window;
+
+static void game_logic() {
+  // Per-frame game logic here
+
+}
+
+static void game_draw(GContext *ctx) {
+  // Per-frame game rendering here
+
+}
+
+static void game_click(int button_id, bool long_click) {
+  // Process click events
+
+}
+
+void pge_init() {
+  // Start the game
+  pge_begin(GColorBlack, game_logic, game_draw, game_click);
+
+  // Keep a Window reference for adding other UI
+  s_game_window = pge_get_window();
+}
+
+void pge_deinit() {
+  // Finish the game
+  pge_finish();
+}
+
+int main(void) {
+  pge_init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_game_window);
+
+  app_event_loop();
+  pge_deinit();
+}
